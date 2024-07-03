41f79fe9a9f62542f6fe7f743c432816424ab321
Lucas Ledsham <lucasnunesleal@gmail.com>
2024-07-03 12:38:51 -0300

    Teste

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 // server.js
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+// Servir arquivos estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Servir arquivos da pasta 'data'
+app.use('/data', express.static(path.join(__dirname, 'data')));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
